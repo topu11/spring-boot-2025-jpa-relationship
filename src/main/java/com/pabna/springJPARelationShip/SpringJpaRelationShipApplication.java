07d9ac8d1ef9840b805aa16b33c4f9078a96f705
topu11 <touhidulislam256@gmail.com>
2025-07-20 22:27:49 +0600

    fresh commit

---
+++ src/main/java/com/pabna/springJPARelationShip/SpringJpaRelationShipApplication.java
@@ -0,0 +1,13 @@
+package com.pabna.springJPARelationShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaRelationShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaRelationShipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pabna/springJPARelationShip/SpringJpaRelationShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabna.springJPARelationShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRelationShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
